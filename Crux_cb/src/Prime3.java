67f548a4fddbc048771ffb60368e7daa9a076995
Pankaj jaiswal <neerajjaiswal@neerajs-MacBook-Air-280.local>
2019-08-12 19:18:49 +0530

    codeupdate

---
+++ Crux_cb/src/Prime3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class Prime3 {
+
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+
+		int n = scn.nextInt();
+
+		int fact = 0;
+
+		int div = 1;
+
+		while (div <= n) {
+
+			if (n % div == 0) {
+				fact++;
+			}
+
+			div = div + 1;
+		}
+
+		if (fact == 2) {
+			System.out.println("Prime");
+		} else {
+			System.out.println("Not Prime");
+		}
+
+	}
+
+}
