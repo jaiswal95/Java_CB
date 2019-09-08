892bfd25f545bb249c17c884ffa5e3b8b30a4b56
Pankaj jaiswal <pankaj1995jaiswal@gmail.com>
2019-09-08 23:07:25 +0530

    codeupdate

--- Crux_java/src/Lecture_9/DisplayArray_ByRecursion.java
+++ Crux_java/src/Lecture_9/DisplayArray_ByRecursion.java
@@ -3,16 +3,16 @@ package Lecture_9;
 import java.util.Scanner;
 
 public class DisplayArray_ByRecursion {
-	
-      static Scanner scn = new Scanner(System.in);
+
+	static Scanner scn = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
+
 		int[] arr = takeInput();
-		
-		
-		
+
+		Display_Array(arr, 0);
 	}
-	
+
 	public static int[] takeInput() {
 
 		int n = scn.nextInt();
@@ -22,7 +22,7 @@ public class DisplayArray_ByRecursion {
 		}
 		return arr;
 	}
-	
+
 	public static void Display_Array(int[] arr, int Virtual_idx) {
 
 		// Base case
@@ -31,6 +31,7 @@ public class DisplayArray_ByRecursion {
 		}
 		// Self work
 		System.out.println(arr[Virtual_idx]);
+		
 		// Samller Problem
 		Display_Array(arr, Virtual_idx + 1);
 
