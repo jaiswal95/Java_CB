d18303a05b8db0e7bf1c91aa89dc531349c9b39a
Pankaj jaiswal <pankaj1995jaiswal@gmail.com>
2019-10-19 23:39:28 +0530

    codeupdate

--- Crux_java/src/Lecture_21/LinkedList.java
+++ Crux_java/src/Lecture_21/LinkedList.java
@@ -12,14 +12,17 @@ public class LinkedList {
 	private Node tail;
 	private int size;
 
+	// O(1)
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	// O(1)
 	public int size() {
 		return size;
 	}
 
+	// O(1)
 	public int getFirst() throws Exception {
 
 		if (size == 0) {
@@ -29,6 +32,7 @@ public class LinkedList {
 		return head.Data;
 	}
 
+	// O(1)
 	public int getLast() throws Exception {
 
 		if (size == 0) {
@@ -38,6 +42,7 @@ public class LinkedList {
 		return tail.Data;
 	}
 
+	// O(n)
 	public int getAt(int idx) throws Exception {
 		if (size == 0) {
 			throw new Exception("Linked List is Empty");
@@ -56,6 +61,7 @@ public class LinkedList {
 		return temp.Data;
 	}
 
+	// O(n)
 	public void Display() {
 		System.out.println("-----------------------");
 		Node temp = head;
@@ -69,6 +75,7 @@ public class LinkedList {
 
 	}
 
+	// O(1)
 	public void AddLast(int item) {
 
 		Node nn = new Node();
@@ -88,6 +95,7 @@ public class LinkedList {
 
 	}
 
+	// O(1)
 	public void AddFirst(int item) {
 
 		Node nn = new Node();
@@ -107,6 +115,7 @@ public class LinkedList {
 
 	}
 
+	// O(n)
 	public Node getNodeAt(int idx) throws Exception {
 		if (size == 0) {
 			throw new Exception("Linked List is Empty");
@@ -125,6 +134,7 @@ public class LinkedList {
 		return temp;
 	}
 
+	// O(n)
 	public void Add_index(int idx, int item) throws Exception {
 
 		if (idx < 0 || idx > size) {
@@ -152,6 +162,7 @@ public class LinkedList {
 
 	}
 
+	// O(1)
 	public int RemoveFirst() throws Exception {
 		if (size == 0) {
 			throw new Exception("Linked List is Empty");
@@ -173,6 +184,7 @@ public class LinkedList {
 
 	}
 
+	// O(n)
 	public int RemoveLast() throws Exception {
 		if (size == 0) {
 			throw new Exception("Linked List is Empty");
@@ -196,6 +208,7 @@ public class LinkedList {
 		return temp;
 	}
 
+	// O(n)
 	public int Remove_idx(int idx) throws Exception {
 		if (size == 0) {
 			throw new Exception("Linked List is Empty");
@@ -221,4 +234,210 @@ public class LinkedList {
 		}
 
 	}
+
+	// RDI
+	public void Reverse_Data_Iteratively() throws Exception {
+		int First = 0;
+		int End = this.size - 1;
+
+		while (First < End) {
+			Node Fn = getNodeAt(First);
+			Node Ln = getNodeAt(End);
+
+			int temp = Fn.Data;
+			Fn.Data = Ln.Data;
+			Ln.Data = temp;
+
+			First++;
+			End--;
+
+		}
+
+	}
+
+	// RPI
+	public void Reverse_Pointers_Iteratively() {
+
+		Node Previous = null;
+		Node Current = head;
+
+		while (Current != null) {
+			Node Ahead = Current.Next_Address;
+			Current.Next_Address = Previous;
+			Previous = Current;
+			Current = Ahead;
+
+		}
+		Node temp = head;
+		head = tail;
+		tail = temp;
+
+	}
+
+	// RPR
+	public void Reverse_Pointers_Recursively() {
+		Reverse_Pointers_Recursively(null, head);
+		Node temp = head;
+		head = tail;
+		tail = temp;
+
+	}
+
+	private void Reverse_Pointers_Recursively(Node Previous, Node Current) {
+		if (Current == null) {
+			return;
+		}
+		Reverse_Pointers_Recursively(Current, Current.Next_Address);
+		Current.Next_Address = Previous;
+
+	}
+
+	// RDR by Return
+	public void Reverse_Data_Recursively() {
+		Reverse_Data_Recursively_ByReturn(head, head, 0);
+	}
+
+	private Node Reverse_Data_Recursively_ByReturn(Node left, Node right, int count) {
+
+		if (right == null) {
+			return left;
+		}
+		Node cl = Reverse_Data_Recursively_ByReturn(left, right.Next_Address, count + 1);
+		if (count >= size / 2) {
+			int temp = cl.Data;
+			cl.Data = right.Data;
+			right.Data = temp;
+
+		}
+		return cl.Next_Address;
+
+	}
+
+	// RDR By Heap
+	private class HeapMover {
+		Node left;
+
+	}
+
+	public void Reverse_Data_Recursively_Heap() {
+
+		HeapMover mover = new HeapMover();
+		mover.left = head;
+		Reverse_Data_Recursively_Heap(mover, head, 0);
+
+	}
+
+	private void Reverse_Data_Recursively_Heap(HeapMover mover, Node right, int count) {
+
+		if (right == null) {
+			return;
+		}
+		Reverse_Data_Recursively_Heap(mover, right.Next_Address, count + 1);
+		if (count >= size / 2) {
+			int temp = mover.left.Data;
+			mover.left.Data = right.Data;
+			right.Data = temp;
+
+		}
+		mover.left = mover.left.Next_Address;
+
+	}
+
+	// Fold
+
+	public void fold() {
+		fold(head, head, 0);
+	}
+
+	private Node fold(Node left, Node right, int count) {
+
+		if (right == null) {
+			return left;
+		}
+
+		Node cl = fold(left, right.Next_Address, count + 1);
+		if (count > size / 2) {
+			Node Ahead = cl.Next_Address;
+			cl.Next_Address = right;
+			right.Next_Address = Ahead;
+			return Ahead;
+		}
+		if (count == size / 2) {
+			tail = right;
+			tail.Next_Address = null;
+		}
+		return null;
+
+	}
+
+	// Mid
+	public int mid() {
+		Node slow = this.head;
+		Node fast = this.head;
+
+		while (fast != null && fast.Next_Address != null) {
+			slow = slow.Next_Address;
+			fast = fast.Next_Address.Next_Address;
+		}
+		return slow.Data;
+	}
+
+	// Kth from Last
+	public int kthlast(int k) {
+
+		Node front = this.head;
+		Node back = this.head;
+		for (int i = 1; i <= k; i++) {
+			front = front.Next_Address;
+		}
+		while (front != null) {
+			front = front.Next_Address;
+			back = back.Next_Address;
+		}
+		return back.Data;
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
--- Crux_java/src/Lecture_21/LinkedList_Client.java
+++ Crux_java/src/Lecture_21/LinkedList_Client.java
@@ -1,29 +1,34 @@
 package Lecture_21;
 
 public class LinkedList_Client {
-	
+
 	public static void main(String[] args) throws Exception {
-		LinkedList list=new LinkedList();
-		
+		LinkedList list = new LinkedList();
+
 		list.AddLast(10);
 		list.AddLast(20);
 		list.AddLast(30);
 		list.AddLast(40);
-		list.Display();
+		//list.Display();
 		list.AddFirst(50);
-		
+
 		list.Display();
 		list.Add_index(list.size(), 70);
+//		list.Display();
+//		
+//		System.out.println(list.Remove_idx(2));
+//		list.Display();
+//		
+//		System.out.println(list.RemoveFirst());
+//		list.Display();
+//		
+//		System.out.println(list.RemoveLast());
 		list.Display();
+
+//		list.Reverse_Pointers_Recursively();
+//		list.Display();
 		
-		System.out.println(list.Remove_idx(2));
-		list.Display();
 		
-		System.out.println(list.RemoveFirst());
-		list.Display();
-		
-		System.out.println(list.RemoveLast());
-		list.Display();
 	}
 
 }
